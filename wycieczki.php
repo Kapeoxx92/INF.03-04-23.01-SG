6eba99610964ac68050a9e16a231564ac5103ca6
1698551 <1698551@TE-GDA-D-D21E10>
2024-11-06 12:27:07 +0100

    first commit

---
+++ wycieczki.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style4.css">
+    <title>Wycieczki po Europie</title>
+</head>
+<body>
+    <header>
+        <h1>BIURO TURYSTYCZNE</h1>
+    </header>
+    <div id="dataBlock">
+        
+    </div>
+    <div id="left">
+
+    </div>
+    <div id="center">
+
+    </div>
+    <div id="right">
+
+    </div>
+</body>
+</html>
