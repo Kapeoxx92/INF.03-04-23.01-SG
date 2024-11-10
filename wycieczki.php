51e309fe3243187c58d39024acbda5b0a3c7a6ce
Kapeoxx92 <kacperwojtczuk92@gmail.com>
2024-11-10 17:03:02 +0100

    secondCommit

--- wycieczki.php
+++ wycieczki.php
@@ -1,26 +1,54 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="styl4.css">
     <title>Wycieczki po Europie</title>
 </head>
+
 <body>
     <header>
         <h1>BIURO TURYSTYCZNE</h1>
     </header>
     <div id="dataBlock">
-        
+        <h3>Wycieczki, na które są wolne miejsca</h3>
+        <ol>
+            skrypt1
+        </ol>
     </div>
-    <div id="left">
-
-    </div>
-    <div id="center">
-
-    </div>
-    <div id="right">
-
-    </div>
+    <main>
+        <div id="left">
+            <h2>Bestselery</h2>
+            <table>
+                <tr>
+                    <th>Wenecja</th>
+                    <th>kwiecień</th>
+                </tr>
+                <tr>
+                    <th>Londyn</th>
+                    <th>lipiec</th>
+                </tr>
+                <tr>
+                    <th>Rzym</th>
+                    <th>wrzesień</th>
+                </tr>
+            </table>
+        </div>
+        <div id="center">
+            <h2>Nasze zdjęcia</h2>
+            skrypt2
+        </div>
+        <div id="right">
+            <h2>Skontaktuj się</h2>
+            <a href="turysta@wycieczki.pl">napisz do nas</a>
+            <p>telefon: 111222333</p>
+        </div>
+    </main>
+    <footer>
+        <p>Stronę opracował: 00000000000</p>
+    </footer>
 </body>
+
 </html>
