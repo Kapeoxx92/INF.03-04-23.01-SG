4804be75c81d29762ecf9b982901a209eae6493e
Kapeoxx92 <kacperwojtczuk92@gmail.com>
2024-11-10 22:41:07 +0100

    finalCommit

--- wycieczki.php
+++ wycieczki.php
@@ -14,9 +14,24 @@
     </header>
     <div id="dataBlock">
         <h3>Wycieczki, na które są wolne miejsca</h3>
-        <ol>
-            skrypt1
-        </ol>
+        <ul>
+            <?php
+            $db = new mysqli('localhost', 'root', '', 'biuro');
+
+            $sql = "SELECT id, dataWyjazdu, cel, cena FROM wycieczki WHERE dostepna= 1";
+            $result = $db->query($sql);
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>";
+                $id = $row['id'];
+                $dzien = $row['dataWyjazdu'];
+                $cel = $row['cel'];
+                $cena = $row['cena'];
+                echo "$id. dnia $dzien do $cel, cena: $cena";
+                echo "</li>";
+            }
+            $db->close()
+            ?>
+        </ul>
     </div>
     <main>
         <div id="left">
@@ -38,7 +53,17 @@
         </div>
         <div id="center">
             <h2>Nasze zdjęcia</h2>
-            skrypt2
+            <?php
+            $db = new mysqli('localhost', 'root', '', 'biuro');
+            $sql = "SELECT nazwaPliku, podpis FROM zdjecia ORDER BY podpis DESC";
+            $result = $db->query($sql);
+            while($row = $result->fetch_assoc()) {
+                $nazwa = $row['nazwaPliku'];
+                $podpis = $row['podpis'];
+                echo "<img src=\"$nazwa\" alt=\"$podpis\">";
+            }
+            $db->close();
+            ?>
         </div>
         <div id="right">
             <h2>Skontaktuj się</h2>
